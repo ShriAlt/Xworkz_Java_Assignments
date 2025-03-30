7309c57447fe21a0d0d81f565d513271509bfbfc
Shriharsha K M <shriharshakm10@gmail.com>
2025-03-30 22:39:48 +0530

    updating

---
+++ enums/src/com/xworkz/enums/Fan.java
@@ -0,0 +1,7 @@
+package com.xworkz.enums;
+
+public class Fan extends Cat {
+public void run(){
+    System.out.println("running fan in fan");
+}
+}
---
+++ enums/src/com/xworkz/enums/FanRunner.java
@@ -0,0 +1,11 @@
+package com.xworkz.enums;
+
+public class FanRunner  {
+    public static void main(String[] args) {
+        Fan fan=new Fan();
+        fan.run();
+        fan.displaycat(CatType.BRITESH_FOLD);
+
+    }
+
+}
