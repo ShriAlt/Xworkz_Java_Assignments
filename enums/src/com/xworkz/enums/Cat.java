46e9c0c494f66f5443f46cbbcd90eba39ee902b7
Shriharsha K M <shriharshakm10@gmail.com>
2025-03-30 22:58:12 +0530

    updating

--- enums/src/com/xworkz/enums/Cat.java
+++ enums/src/com/xworkz/enums/Cat.java
@@ -1,7 +1,18 @@
 package com.xworkz.enums;
 
 public class Cat {
- public void displaycat (CatType catType){
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void displaycat (CatType catType){
      System.out.println("running cat  type");
      System.out.println("cat:"+CatType.BRITESH_FOLD);
      System.out.println("pr0perty:"+CatType.BRITESH_FOLD.getCrazyness());
--- enums/src/com/xworkz/enums/Fan.java
+++ enums/src/com/xworkz/enums/Fan.java
@@ -3,5 +3,13 @@ package com.xworkz.enums;
 public class Fan extends Cat {
 public void run(){
     System.out.println("running fan in fan");
+    super.displaycat(CatType.BRITESH_FOLD);
+    super.setName("harsha");
+    System.out.println(super.getName());
+
+
+}
+ public void displaycat(){
+    System.out.println("overridden");
 }
 }
