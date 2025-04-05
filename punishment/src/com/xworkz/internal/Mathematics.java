d567e58019c269acf920e1a9cea715b8984ec981
Shriharsha K M <shriharshakm10@gmail.com>
2025-04-05 22:54:21 +0530

    updating assignments

--- punishment/src/com/xworkz/internal/Mathematics.java
+++ punishment/src/com/xworkz/internal/Mathematics.java
@@ -1,4 +1,30 @@
 package com.xworkz.internal;
 
 public class Mathematics extends Subject{
+    public Mathematics(){
+        System.out.println("no arg const of Maths");
+    }
+    @Override
+    public void study(){
+        System.out.println("overriding the parent method");
+    }
+    @Override
+    public void takeExam() {
+        System.out.println("Overriding the parent method");
+    }
+
+   @Override
+    public void doAssignment() {
+        System.out.println("Overriding the parent method");
+    }
+
+    @Override
+    public void research() {
+        System.out.println("Overriding the parent method.");
+    }
+    @Override
+    public void attendLecture() {
+        System.out.println("Overriding the parent method");
+    }
 }
+
