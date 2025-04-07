b751dc1d7e656231a68c1c7a815de96c2dfa25c5
Shriharsha K M <shriharshakm10@gmail.com>
2025-04-08 01:21:36 +0530

    updating assignments

--- Rcb/src/com/xworkz/internal/Bed.java
+++ Rcb/src/com/xworkz/internal/Bed.java
@@ -4,15 +4,19 @@ public class Bed {
     public void sleep() {
         System.out.println("Bed is performing sleep.");
     }
+
     public void sit() {
         System.out.println("Bed is performing sit.");
     }
+
     public void makeBed() {
         System.out.println("Bed is performing makeBed.");
     }
+
     public void adjustPosition() {
         System.out.println("Bed is performing adjustPosition.");
     }
+
     public void clean() {
         System.out.println("Bed is performing clean.");
     }
--- Rcb/src/com/xworkz/internal/BedChild.java
+++
@@ -1,7 +0,0 @@
-package com.xworkz.internal;
-
-public class BedChild extends Bed {
-    public void bedSpecialAction() {
-        System.out.println("BedChild is performing a special action.");
-    }
-}
