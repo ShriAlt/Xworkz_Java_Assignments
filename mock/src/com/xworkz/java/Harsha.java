d12890cb434428aedbce0def01d0f07c5684be14
Shriharsha K M <shriharshakm10@gmail.com>
2025-04-26 23:06:43 +0530

    updating

---
+++ mock/src/com/xworkz/java/Harsha.java
@@ -0,0 +1,20 @@
+package com.xworkz.java;
+
+public class Harsha {
+
+    public void count(){
+        int[] nums={ 10,25,30,75,50,20,100,70,60};
+//        int num=10;
+        int result=0;
+        for (int n :nums){
+
+                if ( n>=70 && n <=100){
+                    result ++;
+                }
+//            else System.out.println("no numbers");
+
+        }
+
+        System.out.println(result);
+    }
+}
---
+++ mock/src/com/xworkz/java/runner/MainRunner.java
@@ -0,0 +1,10 @@
+package com.xworkz.java.runner;
+
+import com.xworkz.java.Harsha;
+
+public class MainRunner {
+    public static void main(String[] args) {
+        Harsha harsha=new Harsha();
+        harsha.count();
+    }
+}
