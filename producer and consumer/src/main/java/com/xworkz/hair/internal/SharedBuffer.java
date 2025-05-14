eddaec5347ca1a1337bad3d58bebeef55a8e86a6
Shriharsha K M <shriharshakm10@gmail.com>
2025-05-14 22:48:38 +0530

    update

--- producer and consumer/src/main/java/com/xworkz/hair/internal/SharedBuffer.java
+++ producer and consumer/src/main/java/com/xworkz/hair/internal/SharedBuffer.java
@@ -3,4 +3,8 @@ package com.xworkz.hair.internal;
 public class SharedBuffer {
     private int data;
     private boolean isEmpty=true;
+
+    public synchronized void produce(int value){
+
+    }
 }
