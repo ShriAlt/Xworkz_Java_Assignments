ddc1429c4f0831372bf1c3fa4533bb98cd6a5609
Shriharsha K M <shriharshakm10@gmail.com>
2025-05-16 23:16:38 +0530

    update

--- producer and consumer/src/main/java/com/xworkz/hair/internal/SharedBuffer.java
+++ producer and consumer/src/main/java/com/xworkz/hair/internal/SharedBuffer.java
@@ -4,7 +4,7 @@ public class SharedBuffer {
     private int data;
     private boolean isEmpty=true;
 
-    public synchronized void produce(int value){
+    public synchronized void produce(int value) throws InterruptedException{
 
     }
 }
